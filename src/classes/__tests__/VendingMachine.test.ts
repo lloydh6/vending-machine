a63d4625919a0c9712772d5f07ae0e40f2cbe74a
lloydhesketh <lloyd.hesketh@gmail.com>
2020-05-02 16:09:10 +0100

    altered the vending machine test to take a configuration

--- src/classes/__tests__/VendingMachine.test.ts
+++ src/classes/__tests__/VendingMachine.test.ts
@@ -2,13 +2,16 @@ import { VendingMachine } from '../';
 import { IVendingMachine } from '../../interfaces';
 
 describe('VendingMachine tests', (): void => {
-  let vendingMachine: IVendingMachine;
-
-  beforeEach((): void => {
-    vendingMachine = new VendingMachine();
-  });
-
   it('should initialize a valid vending machine', (): void => {
+    // Arrange
+    const configuration: IVendingMachineConfiguration = {
+
+    };
+
+    // Act
+    const vendingMachine: IVendingMachine = new VendingMachine(configuration);
+
+    // Assert
     expect(vendingMachine).toBeDefined();
   });
 });

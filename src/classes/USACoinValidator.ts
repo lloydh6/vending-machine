5b254eac68bf75e39a619a2739b1d43eec862256
lloydhesketh <lloyd.hesketh@gmail.com>
2020-05-03 09:46:50 +0100

    refactored validate function

--- src/classes/USACoinValidator.ts
+++ src/classes/USACoinValidator.ts
@@ -2,22 +2,21 @@ import { ICoin, ICoinValidator, IValidatedCoin } from '../interfaces';
 import { USACoinWeightEnum, USACoinRadiusEnum, USACoinValuesEnum } from '../enums';
 
 class USACoinValidator implements ICoinValidator {
+
+  private generateValidCoin(coin: ICoin, value: USACoinValuesEnum): IValidatedCoin {
+    return {
+      ...coin,
+      monitoryValue: value,
+    };
+  }
+
   validate(coin: ICoin): IValidatedCoin {
     if (coin.weight === USACoinWeightEnum.dime && coin.radius === USACoinRadiusEnum.dime) {
-      return {
-        ...coin,
-        monitoryValue: USACoinValuesEnum.dime,
-      };
+      return this.generateValidCoin(coin, USACoinValuesEnum.dime);
     } else if (coin.weight === USACoinWeightEnum.nickle && coin.radius === USACoinRadiusEnum.nickle) {
-      return {
-        ...coin,
-        monitoryValue: USACoinValuesEnum.nickle,
-      };
+      return this.generateValidCoin(coin, USACoinValuesEnum.nickle);
     } else if (coin.weight === USACoinWeightEnum.quarter && coin.radius === USACoinRadiusEnum.quarter) {
-      return {
-        ... coin,
-        monitoryValue: USACoinValuesEnum.quarter,
-      };
+      return this.generateValidCoin(coin, USACoinValuesEnum.quarter);
     }
     throw new Error('Invalid Coin');
   }

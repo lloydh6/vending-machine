667eaec62db704f0d70ed1aefce468a2e8bfc6a1
lloydhesketh <lloyd.hesketh@gmail.com>
2020-05-03 09:34:40 +0100

    added some logic to satisfy the test

--- src/classes/USACoinValidator.ts
+++ src/classes/USACoinValidator.ts
@@ -1,7 +1,14 @@
 import { ICoin, ICoinValidator, IValidatedCoin } from '../interfaces';
+import { USACoinWeightEnum, USACoinRadiusEnum, USACoinValuesEnum } from '../enums';
 
 class USACoinValidator implements ICoinValidator {
   validate(coin: ICoin): IValidatedCoin {
+    if (coin.weight === USACoinWeightEnum.dime && coin.radius === USACoinRadiusEnum.dime) {
+      return {
+        ...coin,
+        monitoryValue: USACoinValuesEnum.dime,
+      };
+    }
     throw new Error('Invalid Coin');
   }
 }
